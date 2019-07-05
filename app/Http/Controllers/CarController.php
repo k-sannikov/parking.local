a3c8b1668903e941f8f9bfcca8b444c45aab6eb0
Kirill Sannikov <kir.sannikow@mail.ru>
2019-07-05 14:12:47 +0300

    Создание контроллера

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Car;
+use App\Http\Requests\StoreCarRequest;
+use App\Http\Requests\UpdateCarRequest;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cars = Car::orderBy('place')->get();
+        return view('index', compact('cars'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCarRequest $request)
+    {
+        Car::create($request->all());
+
+        return redirect()->route('car.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Car $car)
+    {
+        return view('show', compact('car'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Car $car)
+    {
+        return view('edit', compact('car'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCarRequest $request, Car $car)
+    {
+        $car->update($request->all());
+
+        return redirect()->route('car.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Car $car)
+    {
+        Car::destroy($car->id);
+        return redirect()->route('car.index');
+    }
+}
